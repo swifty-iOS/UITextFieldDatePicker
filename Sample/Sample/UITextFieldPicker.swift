155763f8d23299152b83ae4e5987cafe5fc48de0
Manish <manishej004@gmail.com>
2017-01-22 23:09:55 +0530

    Lint waring fixed.

--- Sample/Sample/UITextFieldPicker.swift
+++ Sample/Sample/UITextFieldPicker.swift
@@ -8,31 +8,31 @@
 
 import UIKit
 
-enum UITextFieldPickerActionStyle : Int {
+enum UITextFieldPickerActionStyle: Int {
     case `default`
     case close
 }
 
 class UITextFieldPicker: UITextField {
-    
+
     fileprivate var picker: UIPickerView? {
-        didSet{
+        didSet {
             self.inputView = picker
             picker?.reloadAllComponents()
         }
     }
-    
+
     fileprivate var leftButton: PickerButton?
     fileprivate var rightButton: PickerButton?
-    fileprivate var trackSelection :((String?) -> Void)?
-    var dataSet: Array<String> = [] {
+    fileprivate var trackSelection: ((String?) -> Void)?
+    var dataSet: [String] = [] {
         didSet {
             self.picker?.reloadAllComponents()
         }
     }
-    
+
     fileprivate var selectedIndex: Int = -1
-    
+
     weak var pickerDelegate: (UIPickerViewDelegate & UIPickerViewDataSource)? {
         didSet {
             self.picker?.delegate = pickerDelegate ?? self
@@ -40,7 +40,7 @@ class UITextFieldPicker: UITextField {
             self.picker?.reloadAllComponents()
         }
     }
-    
+
     fileprivate var toolBar: UIToolbar {
         guard let inputView = self.inputAccessoryView as? UIToolbar else {
             let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
@@ -50,9 +50,9 @@ class UITextFieldPicker: UITextField {
         }
         return inputView
     }
-    
+
     /* User access properties*/
-    
+
     var autoUpdate = false
     var defaultSelectedString: String?
     var selectedString: String? {
@@ -64,7 +64,7 @@ class UITextFieldPicker: UITextField {
 }
 
 extension UITextFieldPicker {
-    
+
     func setupPicker() {
         self.delegate = self
         let picker = UIPickerView()
@@ -72,54 +72,54 @@ extension UITextFieldPicker {
         picker.dataSource = self
         self.picker = picker
     }
-    
-    func setLeftButton(_ title: String, style: UITextFieldPickerActionStyle, handler:((Void) -> Void)?) {
-        
+
+    func setLeftButton(_ title: String, style: UITextFieldPickerActionStyle, handler: ((Void) -> Void)?) {
+
         let toolbar = self.toolBar
         self.leftButton = PickerButton(title: title, style: .plain, target: self, action:#selector(UITextFieldPicker.buttonAction))
         self.leftButton?.completionBlock = handler
         self.leftButton?.actionStyle = style
         if self.rightButton != nil {
-            toolbar.items = [self.leftButton!, UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),self.rightButton!]
+            toolbar.items = [self.leftButton!, UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil), self.rightButton!]
         } else {
             toolbar.items = [self.leftButton!, UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)]
         }
         toolbar.sizeToFit()
         self.inputAccessoryView = toolbar
     }
-    
-    func setRightButton(_ title: String, style: UITextFieldPickerActionStyle, handler:((Void) -> Void)?) {
-        
+
+    func setRightButton(_ title: String, style: UITextFieldPickerActionStyle, handler: ((Void) -> Void)?) {
+
         let toolbar = self.toolBar
         self.rightButton = PickerButton(title: title, style: .plain, target: self, action:#selector(UITextFieldPicker.buttonAction))
         self.rightButton?.completionBlock = handler
         self.rightButton?.actionStyle = style
-        
+
         if self.leftButton != nil {
-            toolbar.items = [self.leftButton!, UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),self.rightButton!]
+            toolbar.items = [self.leftButton!, UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil), self.rightButton!]
         } else {
-            toolbar.items = [UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),self.rightButton!]
+            toolbar.items = [UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil), self.rightButton!]
         }
         toolbar.sizeToFit()
         self.inputAccessoryView = toolbar
     }
-    
+
     @objc fileprivate func buttonAction(sender: PickerButton) {
         if sender.actionStyle == .close {
             self.closePicker()
         }
         sender.completionBlock?()
     }
-    
-    func showDefaultString(){ self.text = self.defaultSelectedString }
-    
-    func closePicker(){ self.resignFirstResponder() }
-    
+
+    func showDefaultString() { self.text = self.defaultSelectedString }
+
+    func closePicker() { self.resignFirstResponder() }
+
     func trackPickerSelection(handler:@escaping (String?) -> Void) { self.trackSelection = handler }
 }
 
 extension UITextFieldPicker: UIPickerViewDelegate, UIPickerViewDataSource {
-    
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -139,7 +139,7 @@ extension UITextFieldPicker: UIPickerViewDelegate, UIPickerViewDataSource {
 }
 
 extension UITextFieldPicker: UITextFieldDelegate {
-    
+
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         self.selectedIndex = 0
         if let index = self.dataSet.index(of: self.defaultSelectedString ?? "") {
@@ -153,30 +153,29 @@ extension UITextFieldPicker: UITextFieldDelegate {
         }
         return true
     }
-    
+
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
-        
+
     }
 }
 
 extension UITextFieldPicker {
-    
+
     fileprivate struct KeyPath {
         static var Delegate: String { return "delegate" }
     }
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         self.addObserver(self, forKeyPath: KeyPath.Delegate, options: .new, context: nil)
         self.setupPicker()
     }
-    
+
     // Disable paste
     override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
         return action == #selector(UIResponderStandardEditActions.paste(_:)) ? false : super.canPerformAction(action, withSender: sender)
     }
-    
-    
+
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == KeyPath.Delegate {
             self.removeObserver(self, forKeyPath: KeyPath.Delegate, context: nil)
@@ -186,8 +185,7 @@ extension UITextFieldPicker {
     }
 }
 
-
 fileprivate class PickerButton: UIBarButtonItem {
-    var completionBlock: ((Void)-> Void)?
+    var completionBlock: ((Void) -> Void)?
     var actionStyle = UITextFieldPickerActionStyle.default
 }
